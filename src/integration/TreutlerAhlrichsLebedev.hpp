0c497d93b40ab37a4d6ab9625172522d18b68243
Benjamin Curtice Corbett <corbett5@llnl.gov>
2022-09-28 14:13:10 -0700

    Squash

--- src/integration/TreutlerAhlrichsLebedev.hpp
+++ src/integration/TreutlerAhlrichsLebedev.hpp
@@ -72,59 +72,55 @@ auto integrate(
 /**
  *
  */
-template< typename T, typename ATOMIC_BASIS, typename F >
+template< typename T, typename F >
 T evaluateR12Integral(
   ArrayView2d< T const > const & quadratureGrid,
-  CArray< T, 3 > const & r1V,
-  ATOMIC_BASIS const & b2,
-  ATOMIC_BASIS const & b4,
+  Spherical< T > const & r1,
+  ArrayView2d< T const > const & r2BasisValues,
+  IndexType const & b2,
+  IndexType const & b4,
   F && f )
 {
+  IndexType idx = 0;
   return 2 * pi< T > * integrate< 2 >( quadratureGrid, [&] ( CArray< T, 2 > const & r2V )
-  {
-    T const r1 = r1V[ 0 ];
-    T const theta1 = r1V[ 1 ];
-    T const phi1 = r1V[ 2 ];
-    
+  {    
     T const r2 = r2V[ 0 ];
     T const theta2 = r2V[ 1 ];
 
-    T const r12 = calculateR12( r1, theta1, phi1, r2, theta2, T {} );
-
-    T const r2Value = b2.radialComponent( r2 ) * b4.radialComponent( r2 );
-    T const a2ValueMagnitude = sphericalHarmonicMagnitude( b2.l, b2.m, theta2 ) * sphericalHarmonicMagnitude( b4.l, b4.m, theta2 );
-
-    return r2Value * a2ValueMagnitude * f( r1, r2, r12 );
+    T const r12 = calculateR12( r1.r(), r1.theta(), r1.phi(), r2, theta2, T {} );
+    T const answer = r2BasisValues( idx, b2 ) * r2BasisValues( idx, b4 ) * f( r1.r(), r2, r12 );
+    ++idx;
+    return answer;
   } );
 }
 
-template< typename T, typename ATOMIC_BASIS, typename F >
-T evaluateR12Integral(
-  ArrayView2d< T const > const & quadratureGrid,
-  T const r1,
-  Cartesian< T > const & r1C,
-  ATOMIC_BASIS const & b2,
-  ATOMIC_BASIS const & b4,
-  F && f )
-{
-  return 2 * pi< T > * integrate< 2 >( quadratureGrid, [&] ( CArray< T, 2 > const & r12V )
-  {
-    Cartesian< T > const r12C = Spherical< T >{ r12V[ 0 ], r12V[ 1 ], T {} };
+// template< typename T, typename ATOMIC_BASIS, typename F >
+// T evaluateR12Integral(
+//   ArrayView2d< T const > const & quadratureGrid,
+//   T const r1,
+//   Cartesian< T > const & r1C,
+//   ATOMIC_BASIS const & b2,
+//   ATOMIC_BASIS const & b4,
+//   F && f )
+// {
+//   return 2 * pi< T > * integrate< 2 >( quadratureGrid, [&] ( CArray< T, 2 > const & r12V )
+//   {
+//     Cartesian< T > const r12C = Spherical< T >{ r12V[ 0 ], r12V[ 1 ], T {} };
     
-    Cartesian< T > const r2C = r1C + r12C;
+//     Cartesian< T > const r2C = r1C + r12C;
 
-    T const r2 = r2C.r();
-    T const theta2 = std::acos( r2C.z() / (r2 + std::numeric_limits< T >::epsilon()) );
-    // Here we set phi2 to be zero because of an assumed symmetry of the orbitals.
+//     T const r2 = r2C.r();
+//     T const theta2 = std::acos( r2C.z() / (r2 + std::numeric_limits< T >::epsilon()) );
+//     // Here we set phi2 to be zero because of an assumed symmetry of the orbitals.
 
-    T const r12 = r12V[ 0 ];
+//     T const r12 = r12V[ 0 ];
 
-    T const r2Value = b2.radialComponent( r2 ) * b4.radialComponent( r2 );
-    T const a2ValueMagnitude = sphericalHarmonicMagnitude( b2.l, b2.m, theta2 ) * sphericalHarmonicMagnitude( b4.l, b4.m, theta2 );
+//     T const r2Value = b2.radialComponent( r2 ) * b4.radialComponent( r2 );
+//     T const a2ValueMagnitude = sphericalHarmonicMagnitude( b2.l, b2.m, theta2 ) * sphericalHarmonicMagnitude( b4.l, b4.m, theta2 );
 
-    return r2Value * a2ValueMagnitude * f( r1, r1C, r12, r12C, r2 );
-  } );
-}
+//     return r2Value * a2ValueMagnitude * f( r1, r1C, r12, r12C, r2 );
+//   } );
+// }
 
 /**
  * Note: here we are integrating over r1 and r12 using the logarithmic grid, but idk if this is the best in general. It doesn't seem to matter here.
@@ -140,6 +136,8 @@ Array4d< std::complex< T > > integrateAllR1R12(
   
   static_assert( std::is_same_v< T, typename ATOMIC_BASIS::Real > );
 
+  IndexType const nBasis = basisFunctions.size();
+
   QMCCache< 3, T > const qmcCacheR1( nQMC1 );
   QMCCache< 2, T > const qmcCacheR2( nQMC2 );
 
@@ -150,32 +148,55 @@ Array4d< std::complex< T > > integrateAllR1R12(
   Array2d< T > const r1Grid = integration::createGrid( qmcCacheR1,
     changeOfVariables::createMultiple< T, 3 >( rChange, thetaChange, phiChange ) );
 
-  for( IndexType i = 0; i < r1Grid.size( 1 ); ++i )
-  {
-    T const jacobian = std::pow( r1Grid( 0, i ), 2 ) * std::sin( r1Grid( 1, i ) );
-    r1Grid( 3, i ) *= jacobian;
-  }
+  Array2d< std::complex< T > > const r1BasisValues( r1Grid.size(), nBasis );
+  forAll< DefaultPolicy< PolicyType > >( r1Grid.size( 1 ),
+    [&basisFunctions, nBasis, r1Grid=r1Grid.toView(), r1BasisValues=r1BasisValues.toView()] ( IndexType const idx )
+    {
+      T const r = r1Grid( 0, idx );
+      T const theta = r1Grid( 1, idx );
+      T const phi = r1Grid( 2, idx );
+
+      T const jacobian = std::pow( r, 2 ) * std::sin( theta );
+      r1Grid( 3, idx ) *= jacobian;
+
+      for( IndexType b = 0; b < nBasis; ++b )
+      {
+        r1BasisValues( idx, b ) = basisFunctions[ b ]( r, theta, phi );
+      }
+    }
+  );
 
   Array2d< T > const r2Grid = integration::createGrid( qmcCacheR2,
     changeOfVariables::createMultiple< T, 2 >( rChange, thetaChange ) );
   
-  for( IndexType i = 0; i < r2Grid.size( 1 ); ++i )
-  {
-    T const jacobian = std::pow( r2Grid( 0, i ), 2 ) * std::sin( r2Grid( 1, i ) );
-    r2Grid( 2, i ) *= jacobian;
-  }
+  Array2d< T > const r2BasisValues( r2Grid.size(), nBasis );
+  forAll< DefaultPolicy< PolicyType > >( r2Grid.size( 1 ),
+    [&basisFunctions, nBasis, r2Grid=r2Grid.toView(), r2BasisValues=r2BasisValues.toView()] ( IndexType const idx )
+    {
+      T const r = r2Grid( 0, idx );
+      T const theta = r2Grid( 1, idx );
 
-  IndexType nBasis = basisFunctions.size();
+      T const jacobian = std::pow( r, 2 ) * std::sin( theta );
+      r2Grid( 2, idx ) *= jacobian;
 
-  Array4d< std::complex< T > > answer( nBasis, nBasis, nBasis, nBasis );
+      for( IndexType b = 0; b < nBasis; ++b )
+      {
+        // Take the real component because phi2 = 0 so the imaginary component is zero anyways.
+        r2BasisValues( idx, b ) = std::real( basisFunctions[ b ]( r, theta, T {} ) );
+      }
+    }
+  );
+
+  Array4d< std::complex< T > > const answer( nBasis, nBasis, nBasis, nBasis );
 
   forAll< DefaultPolicy< PolicyType > >( r1Grid.size( 1 ),
-    [&basisFunctions, f, r1Grid=r1Grid.toViewConst(), r2Grid=r2Grid.toViewConst(), nBasis, answer=answer.toView()] ( IndexType const idx )
+    [f, r1Grid=r1Grid.toViewConst(), r2Grid=r2Grid.toViewConst(),
+     nBasis, r1BasisValues=r1BasisValues.toViewConst(), r2BasisValues=r2BasisValues.toViewConst(), answer=answer.toView()] ( IndexType const idx )
     {
       Spherical< T > const r1S { r1Grid( 0, idx ), r1Grid( 1, idx ), r1Grid( 2, idx ) };
       T const r1Weight = r1Grid( 3, idx );
 
-      Cartesian< T > const r1C = r1S;
+      // Cartesian< T > const r1C = r1S;
 
       for( IndexType b2 = 0; b2 < nBasis; ++b2 )
       {
@@ -184,23 +205,25 @@ Array4d< std::complex< T > > integrateAllR1R12(
         {
           T const innerIntegral = evaluateR12Integral(
             r2Grid,
-            r1S.r(),
-            r1C,
-            basisFunctions[ b2 ],
-            basisFunctions[ b4 ],
+            r1S,
+            r2BasisValues,
+            b2,
+            b4,
             f );
+
+          // T const innerIntegral = evaluateR12Integral(
+          //   r2Grid,
+          //   r1S.r(),
+          //   r1C,
+          //   basisFunctions[ b2 ],
+          //   basisFunctions[ b4 ],
+          //   f );
           
           for( IndexType b1 = 0; b1 < nBasis; ++b1 )
           {
-            ATOMIC_BASIS const & bf1 = basisFunctions[ b1 ];
             for( IndexType b3 = 0; b3 < nBasis; ++b3 )
             {
-              ATOMIC_BASIS const & bf3 = basisFunctions[ b3 ];
-
-              T const r1Value = bf1.radialComponent( r1S.r() ) * bf3.radialComponent( r1S.r() );
-              std::complex< T > const a1Value = conj( sphericalHarmonic( bf1.l, bf1.m, r1S.theta(), r1S.phi() ) ) * sphericalHarmonic( bf3.l, bf3.m, r1S.theta(), r1S.phi() );
-              
-              std::complex< T > const addition = innerIntegral * r1Weight * a1Value * r1Value;
+              std::complex< T > const addition = innerIntegral * r1Weight * r1BasisValues( idx, b1 ) * r1BasisValues( idx, b3 );
               atomicAdd< PolicyType >( &answer( b1, b2, b3, b4 ), addition );
             }
           }
--- src/unitTests/HartreeFockTest.cpp
+++ src/unitTests/HartreeFockTest.cpp
@@ -39,13 +39,13 @@ int createBasisFunctions(
 TEST( AtomicHartreeFock, Helium_Ochi )
 {
   int const Z = 2;
-  int const nMax = 2;
+  int const nMax = 9;
   int const lMax = 0;
 
   double alpha = 1.35468;
 
-  int const r1GridSize = 3000;
-  int const r2GridSize = 3000;
+  int const r1GridSize = 2000;
+  int const r2GridSize = 2000;
 
   std::vector< OchiBasisFunction< double > > basisFunctions;
   std::vector< AtomicParams > params;
@@ -60,48 +60,58 @@ TEST( AtomicHartreeFock, Helium_Ochi )
   AtomicRCSHartreeFock< double > hfCalculator( 2, params );
   Array1d< double > energies;
 
-  int const nIter = 1;
+  int const nIter = 3;
   for( int iter = 0; iter < nIter; ++iter )
   {
     Array2d< double > const coreMatrix( nBasis, nBasis );
     fillCoreMatrix( coreGrid, Z, basisFunctions, coreMatrix );
     
+    // Array4d< std::complex< double > > twoElectronTerms = integration::integrateAllR1R12< double >( r1GridSize, r2GridSize, basisFunctions,
+    //   [] ( double const r1, Cartesian< double > const & r1C, double const r12, Cartesian< double > const & r12C, double const r2 )
+    //   {
+    //     LVARRAY_UNUSED_VARIABLE( r1 );
+    //     LVARRAY_UNUSED_VARIABLE( r1C );
+    //     LVARRAY_UNUSED_VARIABLE( r12C );
+    //     LVARRAY_UNUSED_VARIABLE( r2 );
+    //     return 1 / r12;
+    //   }
+    // );
+
     Array4d< std::complex< double > > twoElectronTerms = integration::integrateAllR1R12< double >( r1GridSize, r2GridSize, basisFunctions,
-      [] ( double const r1, Cartesian< double > const & r1C, double const r12, Cartesian< double > const & r12C, double const r2 )
+      [] ( double const r1, double const r2, double const r12 )
       {
         LVARRAY_UNUSED_VARIABLE( r1 );
-        LVARRAY_UNUSED_VARIABLE( r1C );
-        LVARRAY_UNUSED_VARIABLE( r12C );
         LVARRAY_UNUSED_VARIABLE( r2 );
         return 1 / r12;
       }
     );
 
-    {
-      double const a = 1.5;
-      Array2d< int > S( 1, 3 );
-      S( 0, 0 ) = 1;
+    // if( false )
+    // {
+    //   double const a = 1.5;
+    //   Array2d< int > S( 1, 3 );
+    //   S( 0, 0 ) = 1;
 
-      Array2d< double > c( 1, 2 );
-      c( 0, false ) = a / 2;
-      c( 0, true ) = a / 4;
+    //   Array2d< double > c( 1, 2 );
+    //   c( 0, false ) = a / 2;
+    //   c( 0, true ) = a / 4;
 
-      jastrowFunctions::Ochi< double > const u { a, a, c, S };
-      Array4d< std::complex< double > > fooBar = integration::integrateAllR1R12< double >( r1GridSize, r2GridSize, basisFunctions,
-        [&u] ( double const r1, Cartesian< double > const & r1C, double const r12, Cartesian< double > const & r12C, double const r2 )
-        {
-          Cartesian< double > const r2C = r1C + r12C;
-          Cartesian< double > r21C {-r12C.x(), -r12C.y(), -r12C.z() };
+    //   jastrowFunctions::Ochi< double > const u { a, a, c, S };
+    //   Array4d< std::complex< double > > fooBar = integration::integrateAllR1R12< double >( r1GridSize, r2GridSize, basisFunctions,
+    //     [&u] ( double const r1, Cartesian< double > const & r1C, double const r12, Cartesian< double > const & r12C, double const r2 )
+    //     {
+    //       Cartesian< double > const r2C = r1C + r12C;
+    //       Cartesian< double > r21C {-r12C.x(), -r12C.y(), -r12C.z() };
 
-          Cartesian< double > const grad1 = u.gradient( r1, r1C, r12, r12C, r2, false );
-          Cartesian< double > const grad2 = u.gradient( r2, r2C, r12, r21C, r1, false );
+    //       Cartesian< double > const grad1 = u.gradient( r1, r1C, r12, r12C, r2, false );
+    //       Cartesian< double > const grad2 = u.gradient( r2, r2C, r12, r21C, r1, false );
 
-          return u.laplacian( r1, r1C, r12, r12C, r2, false ) + u.laplacian( r2, r2C, r12, r21C, r1, false ) - dot( grad1, grad1 ) - dot( grad2, grad2 );
-        }
-      );
+    //       return u.laplacian( r1, r1C, r12, r12C, r2, false ) + u.laplacian( r2, r2C, r12, r21C, r1, false ) - dot( grad1, grad1 ) - dot( grad2, grad2 );
+    //     }
+    //   );
 
-      LVARRAY_LOG_VAR( fooBar[ 0 ][ 0 ][ 0 ] );
-    }
+    //   LVARRAY_LOG_VAR( fooBar[ 0 ][ 0 ][ 0 ] );
+    // }
 
     double energy = hfCalculator.compute( coreMatrix, twoElectronTerms, 100 ).real();
     LVARRAY_LOG_VAR( energy );
